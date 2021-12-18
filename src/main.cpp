af2c0dd9451ad781e9c909e66a665c7646a28537
Todbayar <atodko0513@yahoo.com>
2021-12-18 11:42:29 +0800

    Oled 0.96" u8g2_ssd1306_128x64_nonam_f_hw_i2c

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <Wire.h>
 #include <BH1750.h>
 #include <DS18B20.h>
+#include <U8g2lib.h>
 #include "DHTesp.h"
 #include "gravity_soil_moisture_sensor.h"
 
@@ -14,6 +15,7 @@ BH1750 lightMeter(BH1750_I2C);
 DHTesp dht;
 GravitySoilMoistureSensor soilMoist;
 DS18B20 soilTemp(SOILTEMPPIN);
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C oled(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 void setup() {
     Serial.begin(115200);
@@ -24,9 +26,26 @@ void setup() {
     lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE);
     soilMoist.Setup(SOILPINA);
     Serial.println(soilTemp.getNumberOfDevices());
+
+    oled.begin();
+    oled.enableUTF8Print();
+    //oled.setFont(u8g2_font_4x6_t_cyrillic);
+    oled.setFont(u8g2_font_unifont_t_cyrillic);
+    oled.setFontDirection(0);
 }
 
 void loop() {
+    oled.clearBuffer();
+    oled.setCursor(0, 15);
+    oled.print("Hello World!");
+    oled.setCursor(0, 30);
+    oled.print("Сайн байна уу?");
+    oled.setCursor(0, 45);
+    oled.print("Дэлхий минь!");
+    oled.setCursor(0, 60);
+    oled.print("Ө, Ү");
+    oled.sendBuffer();
+
     if (lightMeter.measurementReady()) {
         float lightVal = lightMeter.readLightLevel();
         Serial.print(F("Light:"));
